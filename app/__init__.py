0e89b2c06e96f0baf6b217f0e64c09e4dc80df92
YVONNEANYANGO <ivonnahbonswuh@gmail.com>
2018-11-26 16:22:20 +0300

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+   
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+   
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    
+    # Will add the views and forms
+    
+    return app 
