9e1171cece1b1638a3400eefd48e73535ba2e241
Theonilah Owali <theonilahtash@gmail.com>
2018-11-28 21:47:03 +0300

    added a flask form for users to add their favorite games and also comment on the existing games

---
+++ app/main/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from ..models import Game, Comment
+from wtforms.validators import DataRequired
+
+
+class GameForm(FlaskForm):
+    title = StringField('Category Games', validators=[DataRequired()])
+    body = TextAreaField('add latest games', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Add a comment', validators=[DataRequired()])
+    submit = SubmitField('Submit')
