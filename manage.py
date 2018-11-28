2a5a5a1ec7016ed8ae29555accfea047c4603539
Theonilah Owali <theonilahtash@gmail.com>
2018-11-28 22:01:17 +0300

    added the tests for login page

--- manage.py
+++ manage.py
@@ -10,6 +10,13 @@ migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 manager.add_command('server', Server)
 
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db)
