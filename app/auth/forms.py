86911a3f8de1dbe696da2bc0310721368d8914cd
Theonilah Owali <theonilahtash@gmail.com>
2018-11-28 21:42:13 +0300

    added a registration form and a login form for users to sign

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
-from ..madels import user
+from ..models import User
 from wtforms import ValidationError
 
 
@@ -12,18 +12,21 @@ class RegistrationForm(FlaskForm):
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
 
+    
 
-class LoginForm(FlaskForm):
-    email = StringField('Your Email Address',validators = [Required(),Email])
-    password = PasswordField('Password',validators = [Required()])
-    remember = BooleanField('Remember me')
-    submit = SubmitField('Sign In')
-
-
-    def validate_email(self,data-field):
+    def validate_email(self,data_field):
         if User.query.filter_by(email = data_field.data).first():
             raise ValidationError('There is an account with that email')
             
             def validate_username(self,data_field):
                 if User.query.filter_by(username = data_field.data).first():
                     raise ValidationError('That username is taken')
+
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators = [Required(),Email()])
+    password = PasswordField('Password',validators = [Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+
